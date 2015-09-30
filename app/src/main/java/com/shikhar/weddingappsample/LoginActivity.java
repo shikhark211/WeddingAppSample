221152f47b6cf385956734249925aee3b5713f08
utkarshnath <nath.utkarsh1@gmail.com>
2015-09-30 12:32:21 +0530

    login

--- app/src/main/java/com/shikhar/weddingappsample/LoginActivity.java
+++ app/src/main/java/com/shikhar/weddingappsample/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.shikhar.weddingappsample;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -17,14 +19,11 @@ import com.facebook.FacebookSdk;
 import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-
-import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -34,7 +33,49 @@ public class LoginActivity extends AppCompatActivity {
     ImageView dp;
     Profile profile = null;
     CallbackManager callbackManager;
+    SharedPreferences sp;
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_login);
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "OCvhok5Gh0FhJhfAA6cKWysZiL53xehGC9zM3OO1", "EmROHXSF6RclFFvsZXu30qv9ZMPbWBI2a25LyCYF");
+        sp = getSharedPreferences("USER", Context.MODE_PRIVATE);
+        textView = (TextView) findViewById(R.id.textview);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, mcallback);
+
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("InviteeProfile");
+//        query.findInBackground(new FindCallback<ParseObject>() {
+//            @Override
+//            public void done(List<ParseObject> list, ParseException e) {
+//                if (e == null) {
+//                    for(int i=0;i<list.size();i++){
+//                        ParseObject inviteeList = new ParseObject("InviteeListinDataBase");
+//                        inviteeList.put("Name", list.get(i).get("Name"));
+//                        inviteeList.put("Id", list.get(i).get("Id"));
+//                        inviteeList.pinInBackground();
+//                        Toast.makeText(getApplicationContext(), inviteeList.get("Name")+"",Toast.LENGTH_SHORT).show();
+//                    }
+//                } else {
+//                    // something went wrong
+//                    textView.setText("ERROR");
+//                }
+//            }
+//        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
     private FacebookCallback<LoginResult> mcallback = new FacebookCallback<LoginResult>() {
         @Override
         public void onSuccess(LoginResult loginResult) {
@@ -44,26 +85,28 @@ public class LoginActivity extends AppCompatActivity {
                 textView.setText("null");
             }
             if(profile!=null){
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("UserName", profile.getFirstName());
+
+                editor.commit();
                 ParseQuery<ParseObject> query = ParseQuery.getQuery("InviteeProfile");
-                query.whereEqualTo("Id", profile.getId());
+                query.whereEqualTo("Id", profile.getLinkUri().toString());
                 query.getFirstInBackground(new GetCallback<ParseObject>() {
                     public void done(ParseObject object, ParseException e) {
                         if (object == null) {
                             ParseObject inviteeProfile = new ParseObject("InviteeProfile");
                             inviteeProfile.put("Name", profile.getName());
-                            inviteeProfile.put("Id", profile.getId());
+                            inviteeProfile.put("Id", profile.getLinkUri().toString());
+                            inviteeProfile.put("fb_dp",profile.getProfilePictureUri(1,1).toString());
                             inviteeProfile.saveInBackground();
                         } else {
-                            Toast.makeText(getApplicationContext(),"already there",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "already there", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-
                 Intent i = new Intent();
                 i.setClass(getApplicationContext(), MainActivity.class);
                 startActivity(i);
-                //textView.setText(" " + profile.getName());
-                //dp.setImageResource(profile.getProfilePictureUri());
             }
         }
 
@@ -77,54 +120,6 @@ public class LoginActivity extends AppCompatActivity {
             textView.setText("err"+e);
         }
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        setContentView(R.layout.activity_login);
-        Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "kkhid8WXxaXR7UZODpKVEKk2JtwRyltLK7jDNUaM", "OXPKSixzrcCnuCIA0GoHgi4g4IjmQuDnrTTYskXC");
-        textView = (TextView) findViewById(R.id.textview);
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("user_friends");
-        callbackManager = CallbackManager.Factory.create();
-        loginButton.registerCallback(callbackManager, mcallback);
-
-//        ParseQuery<ParseObject> query = ParseQuery.getQuery("InviteeProfile");
-//        query.getInBackground("1sfQyji6LG", new GetCallback<ParseObject>() {
-//            public void done(ParseObject object, ParseException e) {
-//                if (e == null) {
-//                    // object will be your game score
-//                    textView.setText(object.getString("Id"));
-//                } else {
-//                    // something went wrong
-//                }
-//            }
-//        });
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("InviteeProfile");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> list, ParseException e) {
-                if (e == null) {
-                    // object will be your game score
-                    textView.setText(list.size() + "");
-                    for(int i=0;i<list.size();i++){
-                        ParseObject inviteeList = new ParseObject("InviteeListinDataBase");
-                        inviteeList.put("Name", list.get(i).get("Name"));
-                        inviteeList.put("Id", list.get(i).get("Id"));
-                        inviteeList.pinInBackground();
-                        Toast.makeText(getApplicationContext(), inviteeList.get("Name")+"",Toast.LENGTH_SHORT).show();
-
-                    }
-                } else {
-                    // something went wrong
-                    textView.setText("ERROR");
-                }
-            }
-        });
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
