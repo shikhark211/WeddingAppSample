251e1921cd33425ffcf796e3d1ee43304c75f2b0
shikhark211 <shikhark211@gmail.com>
2015-09-30 12:48:12 +0530

    login

--- app/src/main/java/com/shikhar/weddingappsample/SplashScreenActivity.java
+++ app/src/main/java/com/shikhar/weddingappsample/SplashScreenActivity.java
@@ -1,28 +1,38 @@
 package com.shikhar.weddingappsample;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        sp = getSharedPreferences("USER", Context.MODE_PRIVATE);
         new Handler().postDelayed(new Runnable()
         {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                Intent mainIntent;
+                if(sp.getString("UserName",null)==null){
+                    mainIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                }else {
+                    mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                }
                 SplashScreenActivity.this.startActivity(mainIntent);
                 SplashScreenActivity.this.finish();
             }
-        }, 3000);
+        }, 1000);
 
     }
 
